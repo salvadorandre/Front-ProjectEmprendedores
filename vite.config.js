37fc3a1ae05d00d3aab954e6b02f84f82ee64c9e
Allan Lacan <allanvasquez1312@gmail.com>
2026-05-02 23:56:02 -0600

    feat: se configuraron caracteristicas de TS para poder trabajar explicitamente con JSX

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath } from "url";
+import path from "path";
 
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -10,4 +14,9 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
+   resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
